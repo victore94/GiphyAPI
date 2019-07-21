933e95c45e54dd1337ba059ce120638c7c361220
Victor Lopez <victorel94@gmail.com>
2019-07-20 22:44:06 -0700

    multiple gifs now append

--- assets/javascript/style.js
+++ assets/javascript/style.js
@@ -5,7 +5,7 @@
 
 function getGif(chosen) {
     // var testing = $("#form-value").val()
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + chosen + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + chosen + "&&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
     // var queryURL = "https://api.giphy.com/v1/gifs/trending?q=&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
@@ -15,31 +15,47 @@ function getGif(chosen) {
         method: "GET"
     }).then(function (response) {
 
-        // var results = response.data
-        // for (var i = 0; i < results.length; i++) {
+        var results = response.data
+        for (var i = 0; i < results.length; i++) {
 
-        // pic = results[i].images.downsized_medium.url
-        // var pic = response.data[0].images.downsized_medium.url
-
-        // $('#imghere').append(`<img src=${pic} />`)
-
-        // }
-        var gifImage = $('<h1>').append(`<img src=${response.data[3].images.downsized_medium.url} />`)
+            var picURL = results[i].images.downsized_medium.url
+            console.log(picURL)
 
 
+
+            // $('#gif-div').empty();
+            $('#gif-div').append(`<img src=${picURL} />`);
+
+            // $('#gif-div').html(gifImage)
+
+
+
+            // // var picture = response.data[i].images.downsized_medium.url
+
+
+
+            // // var gifImage = $('<h1>').append(`<img src=${picture} />`)
+            // $('#gif-div').html(gifImage)
+
+            // pic = results[i].images.downsized_medium.url
+            // var pic = response.data[0].images.downsized_medium.url
+
+
+
+        }
         // $('#gif-div').empty()
-        $('#gif-div').html(gifImage)
+        //  $('#gif-div').append(`<img src=${pic} />`)
+        // $('#gif-div').empty()
 
     })
 }
 
 
-$('#select-gif').on('click', function(e){
+$('#select-gif').on('click', function (e) {
     e.preventDefault();
 
     var inputGif = $('#gif-input').val().trim();
 
-    console.log(e)
     getGif(inputGif)
 
 })
