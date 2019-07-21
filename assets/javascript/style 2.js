5d12128f97236af411b67a9390f7ba9e37f2df34
Victor Lopez <victorel94@gmail.com>
2019-07-21 11:11:55 -0700

    cleaned up code that doesnt work and empty space. still fully functional

---
+++ assets/javascript/style 2.js
@@ -0,0 +1,82 @@
+
+
+
+
+
+function getGif(chosen) {
+    // var testing = $("#form-value").val()
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + chosen + "&&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+
+    // var queryURL = "https://api.giphy.com/v1/gifs/trending?q=&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        var results = response.data
+        for (var i = 0; i < results.length; i++) {
+
+            var picURL = results[i].images.downsized_medium.url
+            console.log(picURL)
+
+
+
+            // $('#gif-div').empty();
+            $('#gif-div').append(`<img src=${picURL} + />`);
+          
+
+
+            // // var picture = response.data[i].images.downsized_medium.url
+
+
+
+            // // var gifImage = $('<h1>').append(`<img src=${picture} />`)
+            // $('#gif-div').html(gifImage)
+
+            // pic = results[i].images.downsized_medium.url
+            // var pic = response.data[0].images.downsized_medium.url
+
+
+
+        }
+        // $('#gif-div').empty()
+        //  $('#gif-div').append(`<img src=${pic} />`)
+        // $('#gif-div').empty()
+
+    })
+}
+
+
+$('#select-gif').on('click', function (e) {
+    e.preventDefault();
+
+    var state = $(this).attr("data-state");
+
+    // $('#gif-div').html(gifImage)
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("#gif-input"));
+        $(this).attr("#gif-input", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("#gif-input", "still");
+      }
+    
+    var inputGif = $('#gif-input').val().trim();
+
+    getGif(inputGif)
+
+})
+
+
+
+
+// $("#select-gif").on("click", function (e) {
+//     e.preventDefault();
+
+//     var testing = $('#gif-input').val()
+//     console.log(testing)
+// })
+
+
---
+++ assets/javascript/style.js
@@ -0,0 +1,25 @@
+function getGif(chosen) {
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + chosen + "&&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var results = response.data
+        for (var i = 0; i < results.length; i++) {
+            var picURL = results[i].images.downsized_medium.url
+            $('#gif-div').append(`<img src=${picURL} + />`);
+        }
+    })
+}
+
+
+$('#select-gif').on('click', function (e) {
+    e.preventDefault();
+    var state = $('#select-gif').attr("#gif-input");
+    console.log(state)
+    var inputGif = $('#gif-input').val().trim();
+    getGif(inputGif)
+})
