2d9f5ea82c8e56f613d3bcd776d0643e17ed1323
Victor Lopez <victorel94@gmail.com>
2019-07-22 10:39:04 -0700

    started my css, added bootstrap

--- assets/javascript/style.js
+++ assets/javascript/style.js
@@ -1,5 +1,6 @@
+$('img').css({'padding': '10px'})
 function getGif(chosen) {
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + chosen + "&&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + chosen + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
 
 
@@ -7,32 +8,64 @@ function getGif(chosen) {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
+        console.log(response)
         var results = response.data
-        $("#gif-div").empty();
+        $(".gif-div").empty();
 
         for (var i = 0; i < results.length; i++) {
+            // this gives me moving images
             var picURL = results[i].images.downsized_medium.url
+             $('.gif-div').append(`<img src=${picURL} + />`);
 
-            $('#gif-div').append(`<img src=${picURL} + />`);
+
+            // //this is for still images
+
+            // var still = $(this).attr('still')
+            // var picURL2 = results[i].images.original_still.url
+            // // console.log(picURL2)
+
+            // $('.gif-div').on('click', function () {
+
+            //     if (still === 'still') {
+            //         console.log('testing')
+            //     } else {
+            //         console.log('clicked')
+            //         $('.gif-div').append(`<img src=${picURL} + />`);
+            //     }
+
+            // })
+
+            // $('.gif-div').append(`<img src=${picURL2} + />`);
+
+
         }
     })
+
+
+
 }
 
 
 $('#select-gif').on('click', function (e) {
     e.preventDefault();
-    $("#gif-div").empty();
-
     var inputGif = $('#gif-input').val().trim();
+    $('.gif-input').val('')
     getGif(inputGif)
 })
 
-
-
 $('.button').on('click', function (e) {
     var myGif = $(this).attr('my-gif')
-    console.log(myGif)
     getGif(myGif)
 })
 
 
+
+
+// // this function will animate the 'still' gif
+// $('.gif-div').on('click', function () {
+//     // this gives you still image
+//     var picURL = data.results[i].images.original_still.url
+//         ('.gif-div').append(`<img src=${picURL} + />`);
+
+//     console.log("testing")
+// })
